f8c757bbead0303d6b89383a0030b1f687664690
MikeVlad1 <130182717+MikeVlad1@users.noreply.github.com>
2025-06-21 07:09:17 +0000

    m

--- script.js
+++ script.js
@@ -22,3 +22,49 @@ function openTab(tabName, elmnt, color) {
 window.onload = function() {
   document.getElementById("defaultOpen").click();
 };
+
+
+
+
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var p;
+
+// Full-width images
+function one() {
+    for (p = 0; p < elements.length; p++) {
+    elements[p].style.msFlex = "100%";  // IE10
+    elements[p].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (p = 0; p < elements.length; p++) {
+    elements[p].style.msFlex = "50%";  // IE10
+    elements[p].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (p = 0; p < elements.length; p++) {
+    elements[p].style.msFlex = "25%";  // IE10
+    elements[p].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var p = 0; p < btns.length; p++) {
+  btns[p].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
